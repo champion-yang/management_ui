e9e7463f483f78183f09366c10957235deeb34e3
yangjunlin <529352969@qq.com>
2020-09-27 22:38:40 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  lintOnSave: false
-}
+
+module.exports={
+  devServer:{
+      proxy:{
+          '/':{            //如果请求路径是"/"
+              target:'http://127.0.0.1:9527', //代理到配置的域名
+              ws:true,          //
+              changeOrigin: true  //当前是否是跨域
+          }
+      }
+  },
+  lintOnSave: false, // 关闭eslint检查
+    publicPath:"./"   //打包目录
+};
